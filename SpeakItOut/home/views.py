1baae31581b3805a2a8a7ef541b1ca88dc835ad3
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-15 11:52:15 +0530

    Added home.html

--- SpeakItOut/home/views.py
+++ SpeakItOut/home/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
-# Create your views here.
+def home(request): 
+    return render(request, "home.html")
